a9f90a6b0f1b747409f844b4faddb6b1e878d123
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2023-12-12 14:40:09 -0500

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,17 @@
+
+#pragma region VEXcode Generated Robot Configuration
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <string.h>
+#include "vex.h"
+
+using namespace vex;
+
+brain Brain;
+
+void playVexcodeSound(const char *soundName);
+
+#pragma endregion VEXcode Generated Robot Configuration
