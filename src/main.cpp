75e440c814365b7db912a0db558babb4a179c9d3
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2023-12-12 15:17:22 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
 
 #include "config.h"
 
+void r_drive_forward() {
+  RightDriveSmart.spinFor(forward, 360, degrees);
+}
+
+void r_drive_backward() {
+  RightDriveSmart.spinFor(reverse, 360, degrees);
+}
+void l_drive_forward() {
+  LeftDriveSmart.spinFor(forward, 360, degrees);
+}
+
+void l_drive_backward() {
+  LeftDriveSmart.spinFor(reverse, 360, degrees);
+}
+
+void controller_handler() {
+  Con.ButtonL1.pressed(l_drive_forward);
+  Con.ButtonL2.pressed(l_drive_backward);
+
+  Con.ButtonR1.pressed(r_drive_forward);
+  Con.ButtonR2.pressed(r_drive_backward);
+}
+
+
 int main() {
-  return 1; // blah blah blah
+  controller_handler(); // handle controller input
 }
