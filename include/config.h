086a5c5a72200ae588523ee7cba1ea55e655147f
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2023-12-12 15:18:55 -0500

    Update config.h

--- include/config.h
+++ include/config.h
@@ -14,4 +14,15 @@ brain Brain;
 
 void playVexcodeSound(const char *soundName);
 
+controller Con;
+motor leftMotorA;
+motor leftMotorB;
+motor_group LeftDriveSmart;
+motor rightMotorA;
+motor rightMotorB;
+motor_group RightDriveSmart;
+drivetrain Drivetrain;
+
+bool RemoteControlCodeEnabled;
+
 #pragma endregion VEXcode Generated Robot Configuration
