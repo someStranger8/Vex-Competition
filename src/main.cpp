6f1ee49a13cd954a7f3a7a5e13ddb2bbc2fd2a0d
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2024-04-02 09:32:46 -0400

    Fixed naming and added random colors for fun

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,16 @@
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
+//#include <time.h> // needed for random func
 
 
 #include "vex.h"
 
 // macros
 // NOTE: avoid magic numbers; use macros
-#define FLYWHEEL_RPM 500
+#define FLYWHEEL_RPM 600 // make fly wheel 600 rpm instead of 500 (600 is the max rpm)
 #define DRIVETRAIN_SPEED 100
+#define WAIT_TIME 3 // how much delay seconds for automation
 
 using namespace vex;
 
@@ -33,67 +35,78 @@ motor flyWheel = motor(PORT2, ratio18_1, true);
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
 
-// "when Controller1 ButtonR1 pressed" hat block
-void onevent_Controller1ButtonR1_pressed_0() {
+
+/*
+  DO NOT TOUCH THE FOLLOWING MOVEMENT FUNCTIONS
+  THEY WILL PROBABLY BREAK
+*/
+
+// right move forward
+void r_mf() {
   while (Controller1.ButtonR1.pressing()) {
     LeftDriveSmart.spin(reverse);
   }
   LeftDriveSmart.stop();
 }
 
-// "when Controller1 ButtonL1 pressed" hat block
-void onevent_Controller1ButtonL1_pressed_0() {
+// left move forward
+void l_mf() {
   while (Controller1.ButtonL1.pressing()) {
     RightDriveSmart.spin(reverse);
   }
   RightDriveSmart.stop();
 }
 
-// "when Controller1 ButtonR2 pressed" hat block
-void onevent_Controller1ButtonR2_pressed_0() {
+// right move backward
+void r_mb() {
   while (Controller1.ButtonR2.pressing()) {
     LeftDriveSmart.spin(forward);
   }
   LeftDriveSmart.stop();
 }
 
-// "when Controller1 ButtonL2 pressed" hat block
-void onevent_Controller1ButtonL2_pressed_0() {
+// left move backward
+void l_mb() {
   while (Controller1.ButtonL2.pressing()) {
     RightDriveSmart.spin(forward);
   } 
   RightDriveSmart.stop();
 }
 
-void onevent_ControllerButtonX_pressed_0() {
+void flywheel_spin() {
+  flyWheel.setVelocity(FLYWHEEL_RPM, rpm); // set velocity before starting
   flyWheel.spin(reverse);
-  flyWheel.setVelocity(FLYWHEEL_RPM, rpm);
 }
 
-void onevent_ControllerButtonB_pressed_0() {
+void flywheel_stop() {
   flyWheel.stop();
 }
 
-// Driver Control
+// driver control setup
 void setupDriver() {
   RightDriveSmart.setVelocity(DRIVETRAIN_SPEED, percent); 
   LeftDriveSmart.setVelocity(DRIVETRAIN_SPEED, percent);
 }
 
-// Controller
+// map input to movement call back funcs
 void handleMovement() {
-  Controller1.ButtonL1.pressed(onevent_Controller1ButtonL1_pressed_0);
-  Controller1.ButtonR1.pressed(onevent_Controller1ButtonR1_pressed_0);
-  Controller1.ButtonL2.pressed(onevent_Controller1ButtonL2_pressed_0);
-  Controller1.ButtonR2.pressed(onevent_Controller1ButtonR2_pressed_0);
+  Controller1.ButtonL1.pressed(l_mf);
+  Controller1.ButtonR1.pressed(r_mf);
+  Controller1.ButtonL2.pressed(l_mb);
+  Controller1.ButtonR2.pressed(r_mb);
 }
 
+// map input to flywheel call back functions
 void handleFlywheel() {
-  Controller1.ButtonX.pressed(onevent_ControllerButtonX_pressed_0); // start
-  Controller1.ButtonB.pressed(onevent_ControllerButtonB_pressed_0); // stop
+  Controller1.ButtonX.pressed(flywheel_spin); // start
+  Controller1.ButtonB.pressed(flywheel_stop); // stop
 }
 
+// driver control callback
 void driver() {
+
+  // why are these seperate?
+
   setupDriver(); 
   handleMovement();
   handleFlywheel();
@@ -101,21 +114,42 @@ void driver() {
 
 // Automation
 void capatalismAtItsPeak() {
+
   LeftDriveSmart.spin(reverse);
   RightDriveSmart.spin(reverse);
-  wait(5, seconds);
+  wait(WAIT_TIME, seconds);
   LeftDriveSmart.stop();
   RightDriveSmart.stop();
 }
 
+void random_colors() {
 
-int main() {
+  // list of random colors
+  const char* colors[8] = {
+    "\033[30m", // black
+    "\033[31m", // red
+    "\033[32m", // green
+    "\033[33m", // yellow
+    "\033[34m", // blue
+    "\033[35m", // purple
+    "\033[36m", // cyan
+    "\033[37m"  // white
+  };
+
+  srand(time(NULL)); // init random
+
+  printf(colors[rand() % 8]); // <- This just changes color of the terminal
+  Brain.Screen.print("Grumio est coquus");
+}
+
+int main(void) {
+
+  // setup callbacks for competition
   competition Competition = competition();
   Competition.drivercontrol(driver);
   Competition.autonomous(capatalismAtItsPeak);
 
+  random_colors();
 
-  // If the program breaks for some reason uncomment the following:
-  // printf("\033[30m");
-  Brain.Screen.print("Grumio est coquus");
+  return 1; // remove this if code breaks
 }
