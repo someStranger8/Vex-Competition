711a4e5435e30919ae2c2fbd199aa07559e4c577
Gage Atkinson <someStranger87@gmail.com>
2024-06-08 11:54:32 -0400

    Code Review

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
 
+// standard libs
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
 
+// vex api
 #include "vex.h"
 
+// define brain
 using namespace vex;
 brain Brain;
 
+// define motor
 motor fun = motor(PORT1, ratio6_1, false);
 
+// main function
 int main() {
-  printf("%s\n", "music");
+
+  // print to screen
   Brain.Screen.print("One must imagine Sisyphus happy");
 
+  // spin forever...
+  // very slowly...
   fun.setVelocity(20, rpm);
   fun.spin(forward);
 
+  // do we need this?
   return 0;
 }
