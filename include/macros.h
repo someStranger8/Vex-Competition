7d4dccb5498d8b44c83f7579b245d881b5cd45e4
Gage Atkinson <someStranger87@gmail.com>
2024-06-10 16:45:24 -0400

    Made  more clean

--- include/macros.h
+++ include/macros.h
@@ -4,6 +4,15 @@
  * NOTE: avoid magic numbers; use macros
 */
 
-#define FLYWHEEL_RPM 600 // make fly wheel 600 rpm instead of 500 (600 is the max rpm)
-#define DRIVETRAIN_SPEED 100 // rpm of the drivetrain motors
-#define WAIT_TIME 3 // how much delay seconds for automation
+
+// flywheel speed (in rpm)
+// 600 is the max rpm of the vex motors
+#define FLYWHEEL_RPM 600
+
+// drivetrain speed (in rpm)
+// have our robot go 100rpm so its not too sensitive
+#define DRIVETRAIN_SPEED 100 
+
+// wait time (in seconds)
+// amount of time to wait for automation
+#define WAIT_TIME 3 
