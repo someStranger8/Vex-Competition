8416ea388cef1b6ae3779b9104d7c7975ac0ae35
Gage Atkinson <someStranger87@gmail.com>
2024-06-09 20:22:37 -0400

    Removed `COLOR_COUNT` from `macros.h`

--- include/macros.h
+++ include/macros.h
@@ -1,6 +1,9 @@
-// macros
-// NOTE: avoid magic numbers; use macros
+
+/*
+ * macros.h
+ * NOTE: avoid magic numbers; use macros
+*/
+
 #define FLYWHEEL_RPM 600 // make fly wheel 600 rpm instead of 500 (600 is the max rpm)
 #define DRIVETRAIN_SPEED 100 // rpm of the drivetrain motors
 #define WAIT_TIME 3 // how much delay seconds for automation
-#define COLOR_COUNT 8 // list of random colors
